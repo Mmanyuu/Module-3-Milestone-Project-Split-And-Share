3bd023f5ad130ab75577444549f1d0905096dbf8
Mmanyuu <omanyu91@gmail.com>
2024-12-12 21:07:00 +0800

    added service test files

--- back-end/src/test/java/sg/edu/ntu/split_and_share/service/UserServiceImplTest.java
+++ back-end/src/test/java/sg/edu/ntu/split_and_share/service/UserServiceImplTest.java
@@ -1,10 +1,13 @@
 package sg.edu.ntu.split_and_share.service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -25,16 +28,36 @@ public class UserServiceImplTest {
     @InjectMocks
     UserServiceImpl userService;
 
-    @Test
+    @Test // Create User Test - Checked Correct
     public void testCreateUser() {
-        User user = User.builder().username("Manyu").password("123456789").name("Mmanyuu").build();
-
+        User user = User.builder().username("Mmanyuu").password("123456789").name("Manyu").build();
         when(userRepository.save(user)).thenReturn(user);
 
         User savedUser = userService.createUser(user);
-
         assertEquals(user, savedUser, "The saved user should be the same as the new customer");
 
         verify(userRepository, times(1)).save(user);
+    }
+
+    @Test
+    public void testInvalidUserCreation() {
+        // invalid can be 1. username, password and name blank, 2.password less than 8
+        // char and 3. username taken
+    }
+
+    @Test
+    public void testGetUser() {
+
+        User user = User.builder().username("Mmanyuu").password("123456789").name("Manyu").build();
+        when(userRepository.findByUsername(user.getUsername())).thenReturn(Optional.of(user));
+        User foundUser = userService.getUser(user.getUsername());
+
+        assertTrue(foundUser.isPresent());
+        assertEquals("Mmanyuu", foundUser.getUsername());
+    }
+
+    @Test
+    public void testUserNotFound() {
+
     }
 }
